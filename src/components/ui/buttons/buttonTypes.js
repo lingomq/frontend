aaa6bd0a19d4da3c8fabe50d30716d73e3af532a
zmqp <81153204+prn-ic@users.noreply.github.com>
2024-05-03 00:58:50 +0500

    Delete src directory

--- src/components/ui/buttons/buttonTypes.js
+++
@@ -1,6 +0,0 @@
-export const buttonTypes = {
-    ALTERNATIVE: "alt",
-    PRIMARY: "primary",
-    WARNING: "warning",
-    ERROR: "error"
-};
--- src/components/ui/modal/ModalSize.js
+++
@@ -1,5 +0,0 @@
-export const modalSize = {
-    SMALL: 'small',
-    AVERAGE: 'average',
-    BIG: 'big'
-}
