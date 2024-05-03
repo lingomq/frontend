83fa58f8c2065f52ae5944491f40951a045b7e56
prn-ic <domarlaid2@mail.ru>
2024-05-03 20:56:06 +0500

    #2-move-angular: добавил поддержку локализации

--- lingomq/src/app/pages/home/home.component.ts
+++ lingomq/src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { LingomqButtonComponent } from '../../core/lingomq-button/lingomq-button.component';
+import { TranslocoModule, TranslocoPipe } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LingomqButtonComponent],
+  imports: [LingomqButtonComponent, TranslocoModule],
+  providers: [TranslocoPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- lingomq/src/app/shared/components/general-header/general-header.component.ts
+++ lingomq/src/app/shared/components/general-header/general-header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { LingomqButtonComponent } from '../../../core/lingomq-button/lingomq-button.component';
+import { TranslocoModule, TranslocoPipe } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-general-header',
   standalone: true,
-  imports: [LingomqButtonComponent],
+  imports: [LingomqButtonComponent, TranslocoModule],
+  providers: [TranslocoPipe],
   templateUrl: './general-header.component.html',
   styleUrl: './general-header.component.scss'
 })
