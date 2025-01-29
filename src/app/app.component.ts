2ac4e8b1fb40f0b6c28f0460a884bef58ea47ea5
sh0rtener <domarlaid2@mail.ru>
2025-01-29 21:42:51 +0500

    refactor - добавил базу

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppThemeService } from './shared/services/app-theme.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'lingomq';
+  constructor() {
+    AppThemeService.useTheme();
+  }
 }
