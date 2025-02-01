4e70523de5ba3adf8b0e35e2b38c5d863b16fbc3
sh0rtener <domarlaid2@mail.ru>
2025-02-01 20:45:04 +0500

    refactor - подправил код css, и добавил страницу 'не найдено'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'not-found',
+    component: NotFoundPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/not-found'
+  }
+];
