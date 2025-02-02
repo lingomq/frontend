0ba496f8aa72047b10da0c0e2354e43895d90752
sh0rtener <domarlaid2@mail.ru>
2025-02-02 08:15:14 +0500

    refactor - добавил landing page

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-import { GeneralFooterComponent } from "../../shared/components/general-footer/general-footer.component";
-import { GeneralHeaderComponent } from "../../shared/components/general-header/general-header.component";
+import { GeneralFooterComponent } from '../../shared/components/general-footer/general-footer.component';
+import { GeneralHeaderComponent } from '../../shared/components/general-header/general-header.component';
+import { TranslocoModule, TranslocoPipe } from '@jsverse/transloco';
+import { LingomqButtonComponent } from "../../core/ui/lingomq-button/lingomq-button.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [GeneralFooterComponent, GeneralHeaderComponent],
+  imports: [GeneralFooterComponent, GeneralHeaderComponent, TranslocoModule, LingomqButtonComponent],
+  providers: [TranslocoPipe],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.scss'
+  styleUrl: './landing.component.scss',
 })
-export class LandingComponent {
-
-}
+export class LandingComponent {}
